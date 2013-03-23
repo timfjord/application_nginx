63277a2b728df4abc21ea3d65b11a2d25d6b02d3
Tima Maslyuchenko <insside@gmail.com>
2013-03-23 20:16:03 +0200

    added metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'application_nginx'
+description      'Chef cookbook for nginx application'
+maintainer       'Tima Maslyuchenko'
+maintainer_email 'insside@gmail.com'
+
+depends          'nginx'
+
+version          '0.0.1'
