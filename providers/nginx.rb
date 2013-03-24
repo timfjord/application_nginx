f18312d4ef039fe0a3ec8f5310bf6200ea167db6
Tima Maslyuchenko <insside@gmail.com>
2013-03-24 12:34:40 +0200

    make sure adding nginx site running in correct run_context

--- providers/nginx.rb
+++ providers/nginx.rb
@@ -1,6 +1,18 @@
 include Chef::Mixin::LanguageIncludeRecipe
 
 action :before_compile do
+end
+
+action :before_deploy do
+end
+
+action :before_migrate do
+end
+
+action :before_symlink do
+end
+
+action :before_restart do
   template "nginx_tempalte_for_#{new_resource.name}" do
     path "#{node[:nginx][:dir]}/sites-available/#{new_resource.name}"
     cookbook new_resource.template ? new_resource.cookbook_name.to_s : 'application_nginx'
@@ -14,20 +26,17 @@ action :before_compile do
     }.merge(new_resource.variables))
   end
   
-  nginx_site new_resource.name
-end
-
-action :before_deploy do
-end
-
-action :before_migrate do
-end
-
-action :before_symlink do
-end
-
-action :before_restart do
+  with_context new_resource.application_provider.run_context do
+    nginx_site new_resource.name
+  end
 end
 
 action :after_restart do
+end
+
+def with_context(context)
+  saved_run_context = @run_context
+  @run_context = context
+  yield
+  @run_context = saved_run_context
 end
